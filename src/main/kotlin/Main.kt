9b5d61101c0825cbb29d54e0451364604d3bb59b
droid-lover <ersachin20@gmail.com>
2025-05-22 17:35:50 +0530

    readline & scanner to take inputs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 package com.nmb
 
+import java.util.Scanner
+
 fun main() {
-    println("Please enter your name:")
-    val name = readLine()
-    println("Your name is $name")
+    println("Please enter a number:")
+    val number = readLine()?.toInt()
+    println("Your number is $number")
+
+
+    println("Please enter a double number:")
+    val scanner = Scanner(System.`in`)
+    val num = scanner.nextDouble()
+    println("Your number is $num")
 }
