e6e17a12480032f39159589ee4cb7d61189e4c21
droid-lover <ersachin20@gmail.com>
2025-05-22 17:41:06 +0530

    comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,13 @@ package com.nmb
 import java.util.Scanner
 
 fun main() {
-    println("Please enter a number:")
-    val number = readLine()?.toInt()
-    println("Your number is $number")
+//    println("Please enter a number:")
+//    val number = readLine()?.toInt()
+//    println("Your number is $number")
 
 
-    println("Please enter a double number:")
+   /* println("Please enter a double number:")
     val scanner = Scanner(System.`in`)
     val num = scanner.nextDouble()
-    println("Your number is $num")
+    println("Your number is $num")*/
 }
